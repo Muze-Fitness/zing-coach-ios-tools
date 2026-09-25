587369390db01698927a3b454916b937e1f06f5f
Mikhail Vospennikov <mikhail.vospennikov@zing.coach>
2026-09-25 14:09:28 +0300

    [DEV-9043] Remove DesignSystem package

--- Package.swift
+++ Package.swift
@@ -6,14 +6,12 @@ let package = Package(
     name: "ZingCoachTools",
     platforms: [.iOS(.v16), .watchOS(.v9)],
     products: [
-        .library(name: "DesignSystem", targets: ["DesignSystem"]),
         .library(name: "LottieAdapter", type: .dynamic, targets: ["LottieAdapter"]),
     ],
     dependencies: [
         .package(url: "https://github.com/airbnb/lottie-ios.git", .upToNextMajor(from: "4.5.0")),
     ],
     targets: [
-        .target(name: "DesignSystem"),
         .target(
             name: "LottieAdapter",
             dependencies: [
